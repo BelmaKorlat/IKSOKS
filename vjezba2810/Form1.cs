46bc956de71f2180441bf7a6416ad563574c1ced
Belma Korlat <137005711+BelmaKorlat@users.noreply.github.com>
2023-10-05 19:21:09 +0200

    Add files via upload

---
+++ vjezba2810/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vjezba2810
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int brojac { get; set; }
+        private void NapraviPotez(object sender)
+        {
+            if(sender is Button)
+            {
+                var dugme = sender as Button;
+                if (dugme.Text == String.Empty)
+                {
+                    if (brojac % 2 == 0)
+                    {
+                        dugme.Text = "X";
+                    }
+                    else
+                    {
+                        dugme.Text = "O";
+                    }
+                    if (brojac % 2 == 0)
+                    {
+                        label1.Text = Igrac1; 
+                    }
+                    else
+                    {
+                        label1.Text = Igrac2;
+                    }
+                }
+            }
+            if (KrajIgre())
+                StatusDugmica(false);
+            brojac++;
+        }
+
+        private void StatusDugmica(bool omoguceno, bool resettext = false, bool resetColor = false)
+        {
+            foreach (var control in Controls)
+            {
+                if (control is Button)
+                {
+                    var kontrola = control as Button;
+                    if (kontrola != button10)
+                    {
+                        kontrola.Enabled = omoguceno;
+                        if (resettext)
+                            kontrola.Text = String.Empty;
+                        if(resetColor)
+                            kontrola.BackColor=default(Color);
+                    }
+                }
+            }
+            brojac = 0;
+        }
+
+        private bool KrajIgre()
+        {
+            return ProvjeriRed() || ProvjeriKolonu() || ProvjeriDijagonalu();
+        }
+        private bool Provjera(Button button1, Button button2, Button button3)
+        {
+            if(button1.Text != "" && button1.Text == button2.Text && button1.Text == button3.Text)
+            {
+                button1.BackColor = button2.BackColor = button3.BackColor = Color.DarkSlateBlue;
+                return true;
+            }
+            return false;
+        }
+
+        private bool ProvjeriDijagonalu()
+        {
+            return Provjera(button1, button5, button9) ||
+              Provjera(button3, button5, button7);
+        }
+
+        private bool ProvjeriKolonu()
+        {
+            return Provjera(button1, button4, button7) ||
+              Provjera(button2, button5, button8) ||
+              Provjera(button3, button6, button9);
+        }
+
+        private bool ProvjeriRed()
+        {
+            return Provjera(button1, button2, button3) ||
+                Provjera(button4, button5, button6) ||
+                Provjera(button7, button8, button9);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NapraviPotez(sender);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NapraviPotez(sender);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            NapraviPotez(sender);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            NapraviPotez(sender);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            NapraviPotez(sender);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            NapraviPotez(sender);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            NapraviPotez(sender);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            NapraviPotez(sender);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            NapraviPotez(sender);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            PonovnoUcitavanjeImena();
+            StatusDugmica(true, true, true);
+        }
+        public string Igrac1 { get; set; }
+        public string Igrac2 { get; set; }
+
+        frmIgraci formica = new frmIgraci();
+        private void PonovnoUcitavanjeImena()
+        {
+            formica.ShowDialog();
+            Igrac1 = formica.Igrac1;
+            Igrac2 = formica.Igrac2;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PonovnoUcitavanjeImena();
+        }
+    }
+}
---
+++ vjezba2810/frmIgraci.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace vjezba2810
+{
+    partial class frmIgraci
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIgrac1 = new System.Windows.Forms.TextBox();
+            this.txtIgrac2 = new System.Windows.Forms.TextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Igrac1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(32, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Igrac2:";
+            // 
+            // txtIgrac1
+            // 
+            this.txtIgrac1.Location = new System.Drawing.Point(161, 41);
+            this.txtIgrac1.Name = "txtIgrac1";
+            this.txtIgrac1.Size = new System.Drawing.Size(148, 26);
+            this.txtIgrac1.TabIndex = 2;
+            // 
+            // txtIgrac2
+            // 
+            this.txtIgrac2.Location = new System.Drawing.Point(161, 95);
+            this.txtIgrac2.Name = "txtIgrac2";
+            this.txtIgrac2.Size = new System.Drawing.Size(148, 26);
+            this.txtIgrac2.TabIndex = 3;
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(213, 145);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(96, 35);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "START";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // frmIgraci
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(371, 203);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.txtIgrac2);
+            this.Controls.Add(this.txtIgrac1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmIgraci";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IGRACI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIgrac1;
+        private System.Windows.Forms.TextBox txtIgrac2;
+        private System.Windows.Forms.Button button10;
+    }
+}
